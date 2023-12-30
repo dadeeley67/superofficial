38f245e2e98f647ab727e9217d03593aa684c9b8
David Deeley <dadeeley67@proton.me>
2023-12-30 14:06:04 -0500

    Changed banner and made nav change colors on scroll

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -2,6 +2,9 @@
 var CSbody = document.querySelector("body");
 const CSnavbarMenu = document.querySelector("#cs-navigation");
 const CShamburgerMenu = document.querySelector("#cs-navigation .cs-toggle");
+const banner = document.querySelector(".scroll-banner");
+
+var containerRect = CSnavbarMenu.getBoundingClientRect();
 
 CShamburgerMenu.addEventListener('click', function () {
 	CShamburgerMenu.classList.toggle("cs-active");
@@ -32,18 +35,58 @@ function ariaExpanded() {
 // and we used these body.scroll styles to create some on scroll 
 // animations with the navbar
 
+let scrollHeight = Math.max(
+	document.body.scrollHeight, document.documentElement.scrollHeight,
+	document.body.offsetHeight, document.documentElement.offsetHeight,
+	document.body.clientHeight, document.documentElement.clientHeight
+);
+
+const scrollChange = scrollHeight / 12;
+
+const step2 = scrollChange;
+const step3 = step2 + scrollChange;
+const step4 = step3 + scrollChange;
+const step5 = step4 + scrollChange;
+const step6 = step5 + scrollChange;
+const step7 = step6 + scrollChange;
+const step8 = step7 + scrollChange;
+const step9 = step8 + scrollChange;
+const step10 = step9 + scrollChange;
+const step11 = step10 + scrollChange;
+const step12 = step11 + scrollChange;
+
 document.addEventListener('scroll', (e) => {
 	const scroll = document.documentElement.scrollTop;
-	if (scroll >= 100) {
+	if (scroll >= 90) {
 		document.querySelector('body').classList.add('scroll')
 	} else {
 		document.querySelector('body').classList.remove('scroll')
 	}
 
-	if (scroll >= 1000) {
-		CSnavbarMenu.classList.add("second-color");
+	if (scroll >= step2 && scroll < step3) {
+		CSnavbarMenu.style.backgroundColor = "#95C3C3";
+	} else if (scroll >= step3 && scroll < step4) {
+		CSnavbarMenu.style.backgroundColor = "#9EBCC1";
+	} else if (scroll >= step4 && scroll < step5) {
+		CSnavbarMenu.style.backgroundColor = "#A6B4C0";
+	} else if (scroll >= step5 && scroll < step6) {
+		CSnavbarMenu.style.backgroundColor = "#AEADBE";
+	} else if (scroll >= step6 && scroll < step7) {
+		CSnavbarMenu.style.backgroundColor = "#B7A6BC";
+	} else if (scroll >= step7 && scroll < step8) {
+		CSnavbarMenu.style.backgroundColor = "#BF9FBA";
+	} else if (scroll >= step8 && scroll < step9) {
+		CSnavbarMenu.style.backgroundColor = "#C898B8";
+	} else if (scroll >= step9 && scroll < step10) {
+		CSnavbarMenu.style.backgroundColor = "#D091B6";
+	} else if (scroll >= step10 && scroll < step11) {
+		CSnavbarMenu.style.backgroundColor = "#D889B5";
+	} else if (scroll >= step11 && scroll < step12) {
+		CSnavbarMenu.style.backgroundColor = "#E182B3";
+	} else if (scroll >= step12) {
+		CSnavbarMenu.style.backgroundColor = "#e97bb1";
 	} else {
-		CSnavbarMenu.classList.remove("second-color");
+		CSnavbarMenu.style.backgroundColor = "#8dcac5";
 	}
 });
 
